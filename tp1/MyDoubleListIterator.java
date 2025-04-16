060ffd1d4abdf0b42fdcf4b91a344b357c8c4d4a
ValentinBulnes <bulnesvalentin@gmail.com>
2025-04-15 21:50:23 -0300

    ej7

---
+++ tp1/MyDoubleListIterator.java
@@ -0,0 +1,48 @@
+package ProgramacionIII.tp1;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class MyDoubleListIterator<T> implements Iterator<T> {
+    private NodeDouble<T> cursor;
+
+    public MyDoubleListIterator(NodeDouble<T> start) {
+        this.cursor = start;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return cursor != null;
+    }
+
+    @Override
+    public T next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+
+        T info = cursor.getInfo();
+        cursor = cursor.getNext();
+        return info;
+    }
+
+    public boolean hasPrevious() {
+        return cursor != null && cursor.getPrev() != null;
+    }
+
+    public T previous() {
+        if (!hasPrevious()) {
+            throw new NoSuchElementException();
+        }
+
+        cursor = cursor.getPrev();
+        return cursor.getInfo();
+    }
+
+    public T get() {
+        if (cursor == null) {
+            throw new NoSuchElementException();
+        }
+        return cursor.getInfo();
+    }
+}

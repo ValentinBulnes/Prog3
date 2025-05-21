ef492a5562085c3fcc72595630122f88a30892d7
ValentinBulnes <bulnesvalentin@gmail.com>
2025-05-21 01:30:40 -0300

    actualizo SimpleLinkedList

--- tp1_EstructurasDeDatos/MySimpleLinkedList.java
+++ tp1_EstructurasDeDatos/MySimpleLinkedList.java
@@ -5,11 +5,12 @@ import java.util.Iterator;
 public class MySimpleLinkedList<T extends Comparable<T>> implements Iterable<T> {
 
 	private Node<T> first;
+	private Node<T> last;
 	private int size;
-
 
 	public MySimpleLinkedList() {
 		this.first = null;
+		this.last = null;
 		this.size = 0;
 	}
 
@@ -25,18 +26,36 @@ public class MySimpleLinkedList<T extends Comparable<T>> implements Iterable<T> 
 			while (aux.getNext()!=null && info.compareTo(aux.getNext().getInfo())>=0) {
 				aux = aux.getNext();
 			}
-				newNode.setNext(aux.getNext());
-				aux.setNext(newNode);
+
+			// Actualizar last si el nuevo nodo es el último
+			if (newNode.getNext() == null) {
+				last = newNode;
 			}
-		this.size++;
+
+			// Solo incrementar size si no se llamó a insertFront()
+			this.size++;
+		}
 	}
 
 	
-	public void insertFront(T info) {
+	public void insertFront(T info) {  // O(1)
 		Node<T> tmp = new Node<T>(info,null);
 		tmp.setNext(this.first);
 		this.first = tmp;
-		this.size = this.size +1;
+		if (size == 0) {
+			last = first;
+		}
+		size++;  // incremento el tamaño de la lista en 1
+	}
+
+	public void insertLast(T info) {  // O(1)}
+		if (size == 0) {
+			this.insertFront(info);
+		}else {
+			last.setNext(new Node<T>(info,null));
+			size++;
+			last = last.getNext();
+		}
 	}
 	
 	public T extractFront() {
@@ -45,6 +64,9 @@ public class MySimpleLinkedList<T extends Comparable<T>> implements Iterable<T> 
 		}
 		Node<T> tmp = this.first;
 		this.first = tmp.getNext();
+		if(this.first == null) { //si al pasar al siguiente es null, el ultimo tambien es null
+			this.last = null;
+		}
 		this.size = this.size - 1;
 		return tmp.getInfo();
 	}
@@ -56,14 +78,10 @@ public class MySimpleLinkedList<T extends Comparable<T>> implements Iterable<T> 
 		return false;
 	}
 	
-	public T get(int index) {
-		if(this.isEmpty() || index < 0 || index >= this.size) {
+	public T get(int index) {     // O(n)
+		if (this.isEmpty() || index < 0 || index >= this.size) {  //si la posicion no existe en la lista devuelvo null
 			return null;
 		}
-		if(index == 0) {
-			return this.first.getInfo();
-		}
-
 		Node<T> tmp = this.first;
 		for(int i = 0; i < index; i++) {
 			tmp = tmp.getNext();

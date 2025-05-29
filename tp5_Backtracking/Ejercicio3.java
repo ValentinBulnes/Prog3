91e013155f321da53867f905db07d8c15dd93c25
ValentinBulnes <bulnesvalentin@gmail.com>
2025-05-28 21:12:52 -0300

    ej3 tp5

---
+++ tp5_Backtracking/Ejercicio3.java
@@ -0,0 +1,40 @@
+package ProgramacionIII.tp5_Backtracking;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Ejercicio3 {
+    private int M = 9; //Objetivo
+    List<List<Integer>> soluciones;
+
+    public List<List<Integer>> backSubconjuntos(List<Integer> conjunto) {
+        soluciones = new ArrayList<>();
+        ArrayList<Integer> auxiliar = new ArrayList<>();
+        backSubconjuntos(conjunto, 0, 0, auxiliar);
+        return soluciones;
+    }
+
+    private void backSubconjuntos(List<Integer> conjunto, Integer actual, Integer suma, ArrayList<Integer> auxiliar) {
+        if (suma == M) { //Caso Base, condicion de corte
+            soluciones.add(new ArrayList<>(auxiliar));
+        } else {
+            for (Integer i = actual; i < conjunto.size(); i++) {
+                auxiliar.add(conjunto.get(i));
+
+                if (suma + conjunto.get(i) <= M) { //Poda
+                    backSubconjuntos(conjunto, i + 1, suma + conjunto.get(i), auxiliar); //Recursion
+                }
+                auxiliar.remove(auxiliar.size() - 1); //Backtrack
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Integer> conjunto = Arrays.asList(4, 6, 2, 1);
+        Ejercicio3 ej3 = new Ejercicio3();
+        List<List<Integer>> resultado = ej3.backSubconjuntos(conjunto);
+
+        System.out.println("Subconjuntos que suman 9: " + resultado);
+    }
+}
